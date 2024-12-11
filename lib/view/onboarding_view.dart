fd5d759a5a7a2d9171cec581c0489b8d5734ba48
coder-aastha <aasthadahal312@gmail.com>
2024-12-11 15:53:21 +0545

    Added title and subtitle for onboarding screen

--- lib/view/onboarding_view.dart
+++ lib/view/onboarding_view.dart
@@ -79,3 +79,34 @@ class _OnboardingViewState extends State<OnboardingView> {
           ],
         ),
       );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 80),
+        child: PageView(
+          controller: controller,
+          onPageChanged: (index) => {setState(() => isLastPage = index == 2)},
+          children: [
+            buildPage(
+                urlImage:
+                    "assets/images/on_boarding_images/onboarding_image1.png",
+                title: "Welcome to Your New Home Journey!",
+                subtitle:
+                    "Discover a world of beautiful homes and exciting opportunities. Let us guide you through every step of the way."),
+            buildPage(
+                urlImage:
+                    "assets/images/on_boarding_images/onboarding_image2.png",
+                title: "Find Your Dream Home",
+                subtitle:
+                    "Browse through our curated listings of stunning properties available for sale. Your perfect home is just a click away."),
+            buildPage(
+                urlImage:
+                    "assets/images/on_boarding_images/onboarding_image3.png",
+                title: "Congratulations! You're Almost Home.",
+                subtitle:
+                    "From browsing to buying, we've helped hundreds of people find their dream homes. Let's make it official!"),
+          ],
+        ),
+      ),
