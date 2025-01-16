5c8a495ef0c018f913ff12f05b830d48f9287858
coder-aastha <aasthadahal312@gmail.com>
2025-01-16 14:32:34 +0545

    Initialized hive database in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:nest_finder/app/app.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:nest_finder/features/auth/data/model/auth_hive_model.dart';
 
-void main() {
+
+void main() async {
+  // Ensure Flutter bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Hive and set up Flutter directory
+  await Hive.initFlutter();
+  
+  // Register the UserModel adapter
+  Hive.registerAdapter(UserModelAdapter());
+  
+  // Open the users box
+  await Hive.openBox<UserModel>('users');
+  
   runApp(
     const MyApp(),
   );
