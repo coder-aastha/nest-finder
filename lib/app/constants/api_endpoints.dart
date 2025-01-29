3d70033c93dc273cd6a150e66aa6ecf142376753
coder-aastha <aasthadahal312@gmail.com>
2025-01-29 17:29:32 +0545

    Added api_endpoints

---
+++ lib/app/constants/api_endpoints.dart
@@ -0,0 +1,11 @@
+class ApiEndpoints {
+  ApiEndpoints._();
+
+  static const Duration connectionTimeout = Duration(seconds: 1000);
+  static const Duration recieveTimeout = Duration(seconds: 1000);
+  static const String baseUrl = "http://localhost:8800/api/";
+
+  //Auth
+  static const String login = "auth/login";
+  static const String register = "auth/register";
+}
