bb2df553a7f11213d2340462ccef10b9af433997
coder-aastha <aasthadahal312@gmail.com>
2025-01-30 11:02:40 +0545

    Modified auth data source

--- lib/features/auth/data/data_source/auth_data_source.dart
+++ lib/features/auth/data/data_source/auth_data_source.dart
@@ -4,7 +4,7 @@ import 'package:nest_finder/features/auth/domain/entity/auth_entity.dart';
 
 
 abstract interface class IAuthDataSource{
-  Future<String> loginUser(String email, String password);
+  Future<String> loginUser(String username, String password);
 
   Future<void> registerUser(AuthEntity user);
 
